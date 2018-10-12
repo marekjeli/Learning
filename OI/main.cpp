df550aa95e96c70ba4b3a6a976ec722b3b548936
Krzysztof Jeliński <krzysztof.jelinski@gmail.com>
2018-10-12 22:58:20 +0200

    Improved formatting and fixed compilation error

--- OI/main.cpp
+++ OI/main.cpp
@@ -2,52 +2,64 @@
 
 using namespace std;
 
-int way=0;
-int place=0;
+int way = 0;
+int place = 0;
 
-int main()
-{
-    int n, t;
-    cin>>n>>t;
-    int t[n];
-    for(int i=0;i<n;++i)
-        cin>>t[i];
-    int x=0,y=0;
-    int xx,yy;
-    cin>>xx>>yy;
-    while(t!=0)
-    {
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<t[i];++j)
-            {
-                if(xx==x&&yy==y)
-                    place+=1;
-                switch (way)
-                {
-                    case 0:
-                    y+=1;
-                    t-=1;
+int main() {
+
+    int n;
+    int t;
+    cin >> n >> t;
+    int tab[n];
+
+    for(int i = 0; i < n; i++) {
+        cin >> tab[i];
+    }
+
+    int x = 0;
+    int y = 0;
+    int xx;
+    int yy;
+    cin >> xx >> yy;
+
+    while(t != 0) {
+
+        for(int i = 0; i < n; i++) {
+
+            for(int j = 0; j < tab[i]; j++) {
+
+                if(xx == x && yy == y) {
+                    place += 1;
+                }
+
+                switch (way) {
+                case 0:
+                    y += 1;
+                    t -= 1;
                     break;
-                    case 1:
-                    x+=1;
-                    t-=1;
+                case 1:
+                    x += 1;
+                    t -= 1;
                     break;
-                    case 2:
-                    y-=1;
-                    t-=1;
+                case 2:
+                    y -= 1;
+                    t -= 1;
                     break;
-                    case 3:
-                    x-=1;
-                    t-=1;
+                case 3:
+                    x -= 1;
+                    t -= 1;
                     break;
                 }
-                if(t==0){break;}
+                if(t == 0) {
+                    break;
+                }
             }
-            way=(way+1)%4;
-            if(t==0){break;}
+            way = (way + 1) % 4;
+            if(t == 0) {
+                break;
+            }
         }
 
     }
-    cout<<place;
+    cout << place;
 }
