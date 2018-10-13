3004f0b88a7c552e57c23c1092322f18d7040089
marekjeli <42449155+marekjeli@users.noreply.github.com>
2018-10-13 23:10:31 +0200

    Update robocikv2_notworking.cpp

--- OI/robocikv2_notworking.cpp
+++ OI/robocikv2_notworking.cpp
@@ -15,6 +15,7 @@ int main() {
     unsigned int ppx=0, pkx=0, ppy=0, pky=0; //pozycja poczatkowa x, pozycja koncowa x, pozycja poczatkowa y, pozycja koncowa y potrzebne do rozpatrzenia zakresu
     unsigned short int direction=0;
     unsigned short place=0;
+    while(t>0){
     for(int i=0;i<n;i++){
             if(t<0){
                 break;
@@ -23,62 +24,68 @@ int main() {
             switch(direction){
             case 0:
                 pky+=tab[i];
-                if(ppx==x && (y<=pky&&y>=ppy) && t>0){
+                if(ppx==x && y<=pky && y>ppy && t>0){
                     place+=1;
                     ppy=pky;
                     }
                     else{
                     int tt=t+tab[i];//zmienna potrzeba zeby program przestal dzialac po tym jak rzeczywiscie sie czas skonczy, gdybym wykorzystal t w przykladzie ponizej program mialby czas dodatni przez co robicik by szedl dalej
-                    if(ppx==x && (y<=pky&&y>=ppy) && tt>=y){
+                    if(ppx==x && y<=pky&&y>ppy && tt>=y){
                         place+=1;
                         }
                     ppy=pky;
                     }
+                break;
 
             case 1:
                 pkx+=tab[i];
-                if(ppy==y && (x<=pkx&&x>=ppx) && t>0){
+                if(ppy==y && x<=pkx&&x>ppx && t>0){
                     place+=1;
                     ppx=pkx;
                     }
                     else{
                     int tt=t+tab[i];
-                    if(ppy==y && (x<=pkx&&x>=ppx) && tt>=x){
+                    if(ppy==y && x<=pkx&&x>ppx && tt>=x){
                         place+=1;
                         }
                     ppx=pkx;
                     }
+                break;
 
             case 2:
                 pky-=tab[i];
-                if(ppx==x && (y>=pky&&y<=ppy) && t>0){
+                if(ppx==x && y>=pky&&y<ppy && t>0){
                     place+=1;
                     ppy=pky;
                     }
                     else{
                     int tt=t+tab[i];
-                    if(ppx==x && (y>=pky&&y>=ppy) && tt>=y){
+                    if(ppx==x && y>=pky&&y>ppy && tt>=y){
                         place+=1;
                         }
                     ppy=pky;
                     }
+                break;
 
             case 3:
                 pkx-=tab[i];
-                if(ppy==y && (x>=pkx&&x<=ppx) && t>0){
+                if(ppy==y && x>=pkx&&x<ppx && t>0){
                     place+=1;
                     ppx=pkx;
                     }
                     else{
                     int tt=t+tab[i];
-                    if(ppy==y && (x>=pkx&&x>=ppx) && tt>=x){
+                    if(ppy==y && x>=pkx&&x>ppx && tt>=x){
                         place+=1;
                         }
                     ppx=pkx;
                     }
+                break;
 
             }
             direction=(direction+1)%4;
+            t--;
+        }
         }
         cout<<place;
 }
